8ed42e0361ffc1cc65fe2f11f4514d714b9e641c
philmcmahon <philip.mcmahon@theguardian.com>
2024-05-23 15:51:07 +0100

    Add simple script to take screenshots of news sites on a schedule

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cloud-scraper
+A simple app to take screenshots of web pages listed in a file and save them to disk.
+
+## Usage
+
+```
+pip install -r requirements.txt
+playwright install
+python screenshot.py
+```
---
+++ screenshot.py
@@ -0,0 +1,41 @@
+import time
+import datetime
+from playwright.sync_api import Page
+from playwright.sync_api import sync_playwright
+
+
+def screenshot_url(page: Page, url: str, screenshot_path: str):
+    page.goto(url)
+    print(screenshot_path)
+    page.screenshot(path=screenshot_path, full_page=True)
+
+
+def read_sites():
+    sites = []
+    with open('sites.txt') as f:
+        sites = f.readlines()
+    return sites
+
+
+def get_hostname(url: str):
+    return url.split("/")[2]
+
+
+def save_screenshots():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        sites = read_sites()
+        iso_time = datetime.datetime.now().isoformat().split(".")[0]
+
+        for site in sites:
+            hostname = get_hostname(site)
+            screenshot_url(page, site, "screenshots/" + hostname + "/" + iso_time + ".png")
+
+        browser.close()
+
+
+while True:
+    save_screenshots()
+    time.sleep(60*60*24)
