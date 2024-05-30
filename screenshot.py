65677beb29d920382b54791ea8f5481f033bca46
philmcmahon <philip.mcmahon@theguardian.com>
2024-05-30 16:56:48 +0200

    Tidy up logging, use string interpolation

--- screenshot.py
+++ screenshot.py
@@ -1,11 +1,14 @@
 import time
 import datetime
+
 from playwright.sync_api import Page
 from playwright.sync_api import sync_playwright
 
+SCREENSHOT_DELAY_SECONDS=60
+
 
 def screenshot_url(page: Page, url: str, screenshot_path: str):
-    print("Screenshotting: " + url + " to " + screenshot_path)
+    print(f'Screenshotting {url} to {screenshot_path}')
     page.goto(url)
     page.screenshot(path=screenshot_path, full_page=True)
 
@@ -27,15 +30,16 @@ def save_screenshots():
         page = browser.new_page()
 
         sites = read_sites()
-        iso_time = datetime.datetime.now().isoformat().split(".")[0]
+        date_time_string = datetime.datetime.now().isoformat().split(".")[0]
 
         for site in sites:
             hostname = get_hostname(site)
-            screenshot_url(page, site, "screenshots/" + hostname + "/" + iso_time + ".png")
+            screenshot_url(page, site, f'screenshots/{hostname}/{date_time_string}.png')
 
         browser.close()
 
-
+print("Starting screenshot scraper")
 while True:
     save_screenshots()
-    time.sleep(60*60*24)
+    print("Next run in " + str(SCREENSHOT_DELAY_SECONDS) + "seconds")
+    time.sleep(SCREENSHOT_DELAY_SECONDS)
